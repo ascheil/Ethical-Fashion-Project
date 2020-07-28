2565c4b8305866fdd1ebd8b01c39ff65cc91c121
Amelie Scheil <amelie.z.scheil@gmail.com>
2020-07-28 14:17:20 -0700

    Initial Commit

---
+++ Ethical Fashion App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ethical Fashion App
+//
+//  Created by Amelie Scheil on 7/28/20.
+//  Copyright © 2020 Amelie Scheil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
