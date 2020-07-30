949e01fa03ca79181e41b70c7ee7c80b18e2dc60
Saanvi Shreesha <saanvi.shreesha@gmail.com>
2020-07-30 12:17:03 -0700

    Saanvi 7/30 12:16pm

--- Ethical Fashion App/ViewController.swift
+++ Ethical Fashion App/ViewController.swift
@@ -19,9 +19,11 @@ class ViewController: UIViewController {
         self.tabBarItem.selectedImage = UIImage(named: "item_selected")
     }
     
+    @IBAction func externalurl1(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://nosidebar.com/ethical-fashion/")! as URL, options: [:], completionHandler: nil)
+    }
+    
     @IBOutlet weak var articleScroll: UIScrollView!
-
-
 
 }
 
